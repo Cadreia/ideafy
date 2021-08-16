95d71d83995e651289b382e92e7ab42c2c72e688
Audrey Ndum <audreyndum@gmail.com>
2021-08-16 03:34:13 +0100

    Updates

---
+++ precache-manifest.0e65ea0117bef84dddfd598f1c702be1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21730c56643275ec93219fb60e34f6c5",
+    "url": "/Cadreia/ideafy/index.html"
+  },
+  {
+    "revision": "599f727a25224ca4cb61",
+    "url": "/Cadreia/ideafy/static/js/2.14ebd957.chunk.js"
+  },
+  {
+    "revision": "134bbfb8114703e52217",
+    "url": "/Cadreia/ideafy/static/js/main.77954fd3.chunk.js"
+  },
+  {
+    "revision": "04acc8fd0270e760c90e",
+    "url": "/Cadreia/ideafy/static/js/runtime-main.8d134d51.js"
+  }
+]);
